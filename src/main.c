73e69696f33ef1a6a31e9440c7955cba31d9d732
KoblizekXD <janprokupek@centrum.cz>
2025-02-08 18:02:37 +0100

    Add definitions for jvm instructions

--- src/main.c
+++ src/main.c
@@ -2,16 +2,15 @@
 #include <classfile/types.h>
 #include <classfile/read.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
     FILE* f = fopen("build/Test.class", "rb"); 
 
     class_file *cf = read_classfile(f);
+    fclose(f);
 
     dump(stdout, *cf);
-    free_classfile(cf);
-    fclose(f);
+    free_classfile(cf); 
     return 0;
 }
